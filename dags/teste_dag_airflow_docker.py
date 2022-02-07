ca945217624f91c128d86dac79dc5e4aa85f399b
Rafael <rafaelchristian2013@gmail.com>
2022-02-07 12:40:59 -0300

    inserindo s3

--- dags/teste_dag_airflow_docker.py
+++ dags/teste_dag_airflow_docker.py
@@ -2,6 +2,9 @@ from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 from airflow import DAG
 import os
+import boto3
+
+
 
 def write_data():
     with open('/opt/airflow/datalake/bronze/readme.txt', 'w') as f:
@@ -9,6 +12,38 @@ def write_data():
 
     with open('/opt/airflow/datalake/silver/readme.txt', 'w') as f:
         f.write('readme')
+
+def upload_s3():
+    with open('/opt/airflow/auxiliary_data/key_id.txt') as f:
+        key_id = f.read()
+
+    with open('/opt/airflow/auxiliary_data/secret_key.txt') as f:
+        secret_key = f.read()
+
+    s3 = boto3.client(
+    's3',
+    aws_access_key_id=key_id,
+    aws_secret_access_key=secret_key,
+    )
+
+    data_bronze = open('/opt/airflow/datalake/bronze/readme.txt', 'rb')
+    s3.put_object(Bucket='datalake-airflow-docker', Key='bronze/readme.txt', Body=data_bronze)
+
+def download_s3():
+    with open('/opt/airflow/auxiliary_data/key_id.txt') as f:
+        key_id = f.read()
+
+    with open('/opt/airflow/auxiliary_data/secret_key.txt') as f:
+        secret_key = f.read()
+
+    s3 = boto3.client(
+                    's3',
+                    aws_access_key_id=key_id,
+                    aws_secret_access_key=secret_key,
+    )
+
+    s3.download_file('datalake-airflow-docker', 'bronze/readme.txt', 'bronze_readme.txt')
+
 
 def remove_data():
     os.remove("/opt/airflow/datalake/bronze/readme.txt")
